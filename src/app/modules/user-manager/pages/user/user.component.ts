679b602cfaf552546481d0b5893804e0c7eb1879
Frame <33612694+flame123flame@users.noreply.github.com>
2024-07-05 00:04:54 +0700

    add user

--- src/app/modules/user-manager/pages/user/user.component.ts
+++ src/app/modules/user-manager/pages/user/user.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../shared/shared.module';
+import { NgZorroAntdModule } from '../../../../shared/ng-zorro-antd.module';
+import { IUser } from '../../interface/user.interface';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [SharedModule,NgZorroAntdModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  users: IUser[] = [];
 
+  isAdmin: boolean = false;
+  isApproved: boolean = false;
+  isUser: boolean = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  
+  }
+
+  addData(): void {
+    // Add logic to add data
+  }
 }
--- src/app/modules/user-manager/services/user.service.ts
+++ src/app/modules/user-manager/services/user.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { User } from '../models/user.model';
 import { IUser } from '../interface/user.interface';
 
 
@@ -11,13 +10,9 @@ import { IUser } from '../interface/user.interface';
   providedIn: 'root'
 })
 export class UserService {
-    private apiUrl = 'https://api.example.com/users'; // URL ของ API ที่ให้ข้อมูล JSON
 
     constructor(private http: HttpClient) {}
 
-    getUsers(): Observable<User[]> {
-        return this.http.get<IUser[]>('/assets/data/users.json').pipe(
-            map(data => data.map(item => new User(item)))
-        );
-    }
-}
+    getUsers(): Observable<IUser[]> {
+        return this.http.get<IUser[]>('/assets/data/users.json');
+      }}

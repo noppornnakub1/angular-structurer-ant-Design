7ccff377f93a8ec80440f7cbe0bfbfa2f4c0fb6b
Frame <33612694+flame123flame@users.noreply.github.com>
2024-07-09 22:11:47 +0700

    add

--- src/app/modules/customer/pages/customer-add/customer-add.component.ts
+++ src/app/modules/customer/pages/customer-add/customer-add.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgZorroAntdModule } from '../../../../shared/ng-zorro-antd.module';
 import { SharedModule } from '../../../../shared/shared.module';
 import {Location} from '@angular/common';
+import { items_province } from '../../../../shared/constants/data-province.constant';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+interface DataLocation {
+  id:number,
+  province: string;
+  district: string;
+  subdistrict: string;
+  postal_code: string;
+}
+
 @Component({
   selector: 'app-customer-add',
   standalone: true,
@@ -9,12 +21,56 @@ import {Location} from '@angular/common';
   templateUrl: './customer-add.component.html',
   styleUrl: './customer-add.component.scss'
 })
-export class CustomerAddComponent {
-  constructor(private _location: Location) 
-  {}
+export class CustomerAddComponent implements OnInit {
+  items_provinces: DataLocation[] = items_province;
+  filteredItemsProvince: DataLocation[] = items_province;
+  customerForm!: FormGroup;
 
+  constructor(private _location: Location, private fb: FormBuilder) {
+ 
+  }
 
-  backClicked() {
+  ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      taxId: [''],
+      address: [''],
+      district: [''],
+      subdistrict: [''],
+      province: [''],
+      postalCode: [''],
+      telephone: [''],
+      email: [''],
+      customerNumber: [''],
+      customerType: [''],
+      site: [''],
+      bankName: ['']
+    });
+  }
+
+  onSearch(value: string): void {
+    this.filteredItemsProvince = this.items_provinces.filter(item => 
+      item.subdistrict.includes(value) ||
+      item.district.includes(value) ||
+      item.province.includes(value) ||
+      item.postal_code.includes(value)
+    );
+  }
+
+  onPostalCodeChange(value: any): void {
+    console.log("value ", value);
+    const selectedItem = this.items_provinces.find(item => item.id === value);
+    if (selectedItem) {
+      this.customerForm.patchValue({
+        district: selectedItem.district,
+        subdistrict: selectedItem.subdistrict,
+        province: selectedItem.province
+      });
+    }
+  }
+
+  backClicked(): void {
     this._location.back();
   }
 }

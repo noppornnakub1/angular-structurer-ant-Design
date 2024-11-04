487db3ba7a15fa27bac2b9511383131383c959cd
Flukelnw <flukelnw1142@gmail.com>
2024-11-04 12:18:13 +0700

    แก้ group bankAdd ไม่แสดง

--- src/app/modules/supplier/pages/supplier-add/supplier-add.component.ts
+++ src/app/modules/supplier/pages/supplier-add/supplier-add.component.ts
@@ -837,6 +837,7 @@ export class SupplierAddComponent {
       this._cdr.detectChanges();
       if (data.supplierBank.length > 0) {
         const bankData = data.supplierBank[0];
+        console.log(data);
 
         if (bankData.supplierGroup && !this.listOfGroup.some(group => group.group_name === bankData.supplierGroup)) {
           this.listOfGroup.push({ group_name: bankData.supplierGroup });
@@ -872,6 +873,7 @@ export class SupplierAddComponent {
         const bankDataAdd = data.supplierBank[1];
         this.isLoadingFromAPI = true;
         this.selectedSupplierGroupAdd = bankDataAdd.SupplierGroup;
+
         this.supplierBankFormAdd.patchValue({
           supbankId: bankDataAdd.SupbankId,
           supplierId: bankDataAdd.SupplierId,
@@ -884,6 +886,7 @@ export class SupplierAddComponent {
         });
 
         this.showSupplierBankFormAdd = true;
+        this._cdr.detectChanges();
         setTimeout(() => {
           this.isLoadingFromAPI = false;
         }, 1000);
@@ -1273,8 +1276,13 @@ export class SupplierAddComponent {
       labelTexts.push(selectedFile.labelText);
     }
 
+    // for (let file of this.fileIdsToRemove) {
+    //   fileIds.push(file.FileId);
+    // } code เก่า
     for (let file of this.fileIdsToRemove) {
-      fileIds.push(file.FileId);
+      if (file.FileId !== undefined) {
+        fileIds.push(file.FileId);
+      }
     }
 
     formData.append('LabelTextsJson', JSON.stringify(labelTexts));
@@ -1560,7 +1568,7 @@ export class SupplierAddComponent {
     });
   }
 
-  getDataCompany( ): void {
+  getDataCompany(): void {
     const CheckcurrentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
     const userCompanies = CheckcurrentUser.company ? CheckcurrentUser.company.split(',') : [];
 
@@ -2060,7 +2068,7 @@ export class SupplierAddComponent {
       name = data.firstname
       this._cdr.markForCheck();
     });
-    if(status === 'Pending Approved By ACC'){
+    if (status === 'Pending Approved By ACC') {
       to = this.emailreq;
       subject = 'OnePortal Notification';
       body = `
@@ -2080,7 +2088,7 @@ export class SupplierAddComponent {
       <p>OnePortal</p>
       <p>กลุ่มบริษัท เดอะ วัน เอ็นเตอร์ไพรส์ จำกัด (มหาชน)</p>`;
     }
-    if(status === 'Pending Approved By ACC'){
+    if (status === 'Pending Approved By ACC') {
       to = this.emailreq;
       subject = 'OnePortal Notification';
       body = `
@@ -2103,7 +2111,7 @@ export class SupplierAddComponent {
       <p>OnePortal</p>
       <p>กลุ่มบริษัท เดอะ วัน เอ็นเตอร์ไพรส์ จำกัด (มหาชน)</p>`;
     }
-    else if(status === 'Approved By ACC' || status === 'Reject By ACC'){
+    else if (status === 'Approved By ACC' || status === 'Reject By ACC') {
       to = this.emailreq;
       subject = 'OnePortal Notification';
       body = `
@@ -2123,7 +2131,7 @@ export class SupplierAddComponent {
       <p>OnePortal</p>
       <p>กลุ่มบริษัท เดอะ วัน เอ็นเตอร์ไพรส์ จำกัด (มหาชน)</p>`;
     }
-    else if(status === 'Reject By FN'){
+    else if (status === 'Reject By FN') {
       to = this.emailreq;
       subject = 'OnePortal Notification';
       body = `
@@ -2143,7 +2151,7 @@ export class SupplierAddComponent {
       <p>OnePortal</p>
       <p>กลุ่มบริษัท เดอะ วัน เอ็นเตอร์ไพรส์ จำกัด (มหาชน)</p>`;
     }
-    else if(status === 'Approved By FN'){
+    else if (status === 'Approved By FN') {
       to = this.emailreq;
       subject = 'OnePortal Notification';
       body = `
@@ -2164,7 +2172,7 @@ export class SupplierAddComponent {
       <p>OnePortal</p>
       <p>กลุ่มบริษัท เดอะ วัน เอ็นเตอร์ไพรส์ จำกัด (มหาชน)</p>`;
     }
-    
+
 
     this.emailService.sendEmail(to, subject, body).subscribe(
       (response) => {

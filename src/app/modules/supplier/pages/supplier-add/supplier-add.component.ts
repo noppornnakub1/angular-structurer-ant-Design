b27b2dbed5c784202856d93793c45d4296c1c8cd
Flukelnw <flukelnw1142@gmail.com>
2025-08-27 10:45:11 +0700

    fix branch bank 9999

--- src/app/modules/supplier/pages/supplier-add/supplier-add.component.ts
+++ src/app/modules/supplier/pages/supplier-add/supplier-add.component.ts
@@ -285,7 +285,7 @@ export class SupplierAddComponent {
   private createBankFormGroup(): FormGroup {
     return this.fb.group({
       supbankId: [0], supplierId: [, Validators.required],
-      nameBank: ['', Validators.required], branch: ['00000', Validators.required],
+      nameBank: ['', Validators.required], branch: ['9999', Validators.required],
       accountNum: ['', Validators.required], supplierGroup: ['', Validators.required],
       accountName: ['', Validators.required], company: ['', Validators.required]
     });

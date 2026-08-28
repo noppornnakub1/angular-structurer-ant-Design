4ac4b9963edd2170adbb187b1f30504985c46475
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2026-08-28 10:42:03 +0700

    fix แสดงสถานะ สำหรับ การ action ของ Fn

--- src/app/modules/dashboard/pages/dashboard/view-details/view-details.component.ts
+++ src/app/modules/dashboard/pages/dashboard/view-details/view-details.component.ts
@@ -1,19 +1,53 @@
-import { Component, Input } from '@angular/core';
-import { ModalDataService } from '../../../services/modal-data.service';
-import { CommonModule } from '@angular/common';
-
+import { Component, Input } from "@angular/core";
+import { ModalDataService } from "../../../services/modal-data.service";
+import { CommonModule } from "@angular/common";
+import { SupplierService } from "../../../../supplier/services/supplier.service";
+import { CustomerService } from "../../../../customer/services/customer.service";
 
 @Component({
-  selector: 'app-view-details',
+  selector: "app-view-details",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './view-details.component.html',
-  styleUrl: './view-details.component.scss'
+  templateUrl: "./view-details.component.html",
+  styleUrl: "./view-details.component.scss",
 })
 export class ViewDetailsComponent {
   data: any;
+  type: string = "";
+  logs: any[] = [];
+  constructor(
+    private modalDataService: ModalDataService,
+    private supplierService: SupplierService,
+    private customerService: CustomerService,
+  ) {
+    this.data = this.modalDataService.getData();
+    this.type = this.data?.source || "";
 
-  constructor(private modalDataService: ModalDataService) {
-    this.data = this.modalDataService.getData();    
+    if (this.type === "Supplier") {
+      this.supplierService.getLog(this.data.id).subscribe((log) => {
+        this.logs = log;
+      });
+    } else if (this.type === "Customer") {
+      this.customerService.getLog(this.data.id).subscribe((log) => {
+        this.logs = log;
+      });
+    }
+  }
+
+  fnApproverStatus(): string {
+    const status = this.data?.status;
+    if (status === "Success") {
+      return "(Success)";
+    }
+    if (status === "Approved By ACC") {
+      return "(Waiting FN Review)";
+    }
+    if (status === "Pending Approved By ACC") {
+      const fnLog = this.logs.find((log) => log.userId === this.data?.ownerFN);
+      if (fnLog && fnLog.rejectReason !== "" && fnLog.rejectReason !== null) {
+        return "(Reject)";
+      }
+    }
+    return "";
   }
 }
